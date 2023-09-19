613dafb2dc68a76d463717deee275c9b7acfde6e
josemtx <josemataixperez@gmail.com>
2023-09-19 11:19:13 +0100

    First commit.

--- 1. Fundamentals/Structure/Classes in Java/src/ClassesInJava.java
+++ 1. Fundamentals/Structure/Classes in Java/src/ClassesInJava.java
@@ -1,6 +1,7 @@
 //The class, as well as the file, is called ClassesInJava.
 public class ClassesInJava
 {
+
    //The main method; put it as-is in all your codes for now.
    public static void main(String[] args)
    {
